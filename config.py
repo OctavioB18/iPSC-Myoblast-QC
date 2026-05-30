6cfa8219f7c157e4541df6d562f0824bf65ad085
Octavio B <octavio.nunes@usp.br>
2026-05-30 16:54:22 -0300

    Delete config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-APP_TITLE = "iPSC-Myoblast Image QC & Annotation"
-SUPPORTED_EXTENSIONS = {".png", ".jpg", ".jpeg", ".tif", ".tiff", ".bmp"}
-DEFAULT_OUTPUT_DIR = "outputs"
-DEFAULT_IMAGE_DIR = "images"
